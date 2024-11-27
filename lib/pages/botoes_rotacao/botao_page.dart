e297c58a580ea04a2a28ca0d39dd14ad7e775b7a
MizaelDouglasDeMello <mizaeldouglas4@gmail.com>
2024-11-26 21:05:33 -0300

    feat:  buttons

--- lib/pages/botoes_rotacao/botao_page.dart
+++ lib/pages/botoes_rotacao/botao_page.dart
@@ -18,10 +18,29 @@ class _BotaoPageState extends State<BotaoPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              RotatedBox(
+                  quarterTurns: -1,
+                  child: TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                        maximumSize: const Size(200, 50),
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.blue,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20))),
+                    child: const Text("RotatedBox"),
+                  )),
               ElevatedButton(
                 onPressed: () {
                   // Add onPressed code here
                 },
+                style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: Colors.red,
+                    shadowColor: Colors.black,
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20))),
                 child: Text('ElevatedButton'),
               ),
               TextButton(
